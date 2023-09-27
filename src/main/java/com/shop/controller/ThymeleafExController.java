c431755f0217d243411dfa83bf3790f1b2c75c22
lee jihyeon <lejinhy@gmail.com>
2023-09-27 18:09:29 +0900

    feat: spring boot devtools 설정

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 // RequestMapping : 클라이언트의 요청을 어떤 컨트롤러가 처리할지 매핑. 이 경우 타임리프 경로로 오는 요청을 ThymeleafExController가 처리함
 @RequestMapping(value = "/thymeleaf")
 public class ThymeleafExController {
-    @GetMapping(value = "/ex01")
+    @GetMapping(value = "" +
+            "/ex01")
     public String thymeleafExample01(Model model) {
         model.addAttribute("data", "타임리프 예제 입니다."); // 뷰에 전달할 데이터를 모델 객체를 이용해 key,value 구조로 넣어준다.
         return "thymeleafEx/thymeleafEx01"; // templates 폴더를 기준으로 뷰의 위치와 이름(thymeleafEx01.html)을 반환.
