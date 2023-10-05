d05ddeb65c97f7d6b0a87795d7e308fbff6f7a76
lee jihyeon <lejinhy@gmail.com>
2023-10-06 00:18:53 +0900

    refactor: SecurityConfig 주석 추가

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -8,15 +8,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * WebSecurityConfigurerAdapter를 상속받는 클래스에 @EnableWebSecurity 어노테이션을 선언하면 SpringSecurityFilterChain이 자동으로 포함된다.
+ * 메소드 오버라이딩을 통해 보안 설정을 커스터마이징 할 수 있다.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    /*
+     * http 요청에 대한 보안을 설정한다.
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
     }
 
+    /*
+     * BCryptPasswordEncoder의 해시함수를 사용해서 비밀번호를 암호화하여 저장.
+     * BCryptPasswordEncoder를 빈으로 등록해서 사용한다.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
