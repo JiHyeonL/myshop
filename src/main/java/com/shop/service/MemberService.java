0bcb7572a9abcf38c7b5c44949c922fcf92284b8
lee jihyeon <lejinhy@gmail.com>
2023-10-09 23:51:15 +0900

    feat: 비즈니스 로직 담당 서비스 패키지 추가, 회원가입 기능 메소드

---
+++ src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,28 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+
+@Service    // 비즈니스 로직 담당하는 서비스 계층.
+@Transactional  // 로직을 처리하다가 에러가 발생하였다면, 변경된 데이터를 로직을 수행하기 이전 상태로 콜백 시켜준다.
+@RequiredArgsConstructor    // final이나 @NonNull이 붙은 필드에 생성자를 생성한다.
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
