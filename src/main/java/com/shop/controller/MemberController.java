274d4b52ee37444cdb0fd9ecb89c86b3f2d3fb56
lee jihyeon <lejinhy@gmail.com>
2023-10-12 23:44:56 +0900

    feat: 유효하지 않은 회원가입 시 해당 이유를 화면에서 보여주는 기능 추가

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -34,6 +37,28 @@ public class MemberController {
 
         Member member = Member.createMember(memberFormDto, passwordEncoder);
         memberService.saveMember(member);
+
+        return "redirect:/";
+    }
+
+    /**
+     * 검증하려는 객체인 memberFormDto 앞에 @Valid 를 붙임.
+     * 검사 후 결과는 bindingResult에 담아 준 뒤,
+     * bindingResult.hasError를 호출하여 에러가 있다면 회원가입 페이지로 이동.
+     */
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
